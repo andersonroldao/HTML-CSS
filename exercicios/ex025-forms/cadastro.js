14bb36a90d879e992dbfbb7cbae882e52c56c6ae
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-07-09 12:48:28 -0300

    Update cadastro.js

--- exercicios/ex025-forms/cadastro.js
+++
@@ -1,8 +0,0 @@
-function enviar(params) {
-    var nome = document.getElementById("inome");
-    var sobrenome = document.getElementById("isobrenome");
-
-    if (nome.value != "" && sobrenome.value != "") {
-        alert(`Obrigado sr(a) ${nome.value} ${sobrenome.value} os seus dados foram encaminhados com sucesso.`)
-    }
-}
