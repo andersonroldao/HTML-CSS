e44694eaa653328fccb1a13aa018e422838f49af
Anderson <66678707+andersonroldao@users.noreply.github.com>
2022-07-28 11:01:49 -0300

    Delete cadastro.js

--- exercicios/ex025-forms/cadastro.js
+++
@@ -1,8 +0,0 @@
-let usuario = document.getElementById('usuario')
-let senha = document.getElementById('senha')
-
-function enviar() {
-    if (usuario.value.length >= 5 && senha.value.length >= 8) {
-        alert(`${usuario.value}, cadastro efetuado com sucesso.`)
-    }
-}
